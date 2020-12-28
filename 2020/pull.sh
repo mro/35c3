101705b8704b8ca7c7a0c0ae3db32843dc13c2c5
Marcus Rohrmoser <work@mro.name>
2020-12-28 17:49:50 +0100

    🚀

--- 2020/pull.sh
+++ 2020/pull.sh
@@ -2,39 +2,39 @@
 cd "$(dirname "${0}")" || exit 1
 #
 # See
-# - https://events.ccc.de/${evt}/2018/wiki/Static:Crawling
+# - https://events.ccc.de/${evet}/2018/wiki/Static:Crawling
 # - https://code.mro.name/mro/35c3/
 #
 
 USER_AGENT="https://code.mro.name/mro/c3"
 dir=Fahrplan
-evt=congress
-evt=rc3
+evet=congress
+evet=rc3
 year=2020
 
-# url="https://${dir}.events.ccc.de/${evt}/2015/${dir}/version"
-url="https://${dir}.events.ccc.de/${evt}/${year}/${dir}/version"
+# url="https://${dir}.events.ccc.de/${evet}/2015/${dir}/version"
+url="https://${dir}.events.ccc.de/${evet}/${year}/${dir}/version"
 dst="${dir}.version"
 
 {
-  curl --output "${dir}/schedule.xml" --location "https://fahrplan.events.ccc.de/${evt}/${year}/${dir}/schedule.xml"
+  curl --output "${dir}/schedule.xml" --location "https://fahrplan.events.ccc.de/${evet}/${year}/${dir}/schedule.xml"
   {
     echo '<?xml-stylesheet type="text/xsl" href="../assets/schedule2html.xslt"?>'
     grep -vF "<?xml version=" "${dir}/schedule.xml"
   } | xmllint --output "${dir}"/schedule2.xml --relaxng assets/schedule.rng --format --encode utf-8 -
-  sed -i -e "s|<url>https://fahrplan.events.ccc.de/${evt}/${year}/Fahrplan/events/|<url>./events/|g" "${dir}"/schedule2.xml
+  sed -i -e "s|<url>https://fahrplan.events.ccc.de/${evet}/${year}/Fahrplan/events/|<url>./events/|g" "${dir}"/schedule2.xml
 }
 
 for evt in $(grep -F '<url>' Fahrplan/schedule2.xml | grep -hoE '[0-9]+' | sort -n)
 do 
   dst_evt="${dir}/events/${evt}.html"
-  url_evt="https://fahrplan.events.ccc.de/${evt}/${year}/${dst_evt}"
+  url_evt="https://fahrplan.events.ccc.de/${evet}/${year}/${dst_evt}"
 	echo "${url_evt}"
   curl --silent --max-time 3 --create-dirs --location --remote-time --time-cond "${dst_evt}" --output "${dst_evt}" "${url_evt}"
 done
 
 {	
-  curl --output "${dir}/schedule.json" --location "https://fahrplan.events.ccc.de/${evt}/${year}/${dir}/schedule.json"
+  curl --output "${dir}/schedule.json" --location "https://fahrplan.events.ccc.de/${evet}/${year}/${dir}/schedule.json"
   ruby -ryaml -e "puts YAML::dump(YAML::load(STDIN.read))" < "${dir}/schedule.json" > "${dir}/schedule.yaml"
 }
 
@@ -45,8 +45,8 @@ curl --silent --location --remote-time --output "${dst}" --time-cond "${dst}" --
   curl --silent --location --remote-time --output "${dst}" --time-cond "${dst}" --user-agent "${USER_AGENT}" "${url}" && {
     rm -rf "${dir}"
     tar -xzf "${dst}" && mv rC3 "${dir}"
-    sed -i -e "s:/${evt}/${year}/${dir}/:./:g" "${dir}"/*.html
-    sed -i -e "s:/${evt}/${year}/${dir}/:../:g" "${dir}"/*/*.html
+    sed -i -e "s:/${evet}/${year}/${dir}/:./:g" "${dir}"/*.html
+    sed -i -e "s:/${evet}/${year}/${dir}/:../:g" "${dir}"/*/*.html
 
     {
       echo '<?xml-stylesheet type="text/xsl" href="../assets/schedule2html.xslt"?>'
@@ -64,7 +64,7 @@ curl --silent --location --remote-time --output "${dst}" --time-cond "${dst}" --
 for part in everything workshops
 do
   dst="${dir}/${part}.schedule.xml"
-  url="https://${dir}.events.ccc.de/${evt}/${year}/${dst}"
+  url="https://${dir}.events.ccc.de/${evet}/${year}/${dst}"
   curl --silent --location --remote-time --output "${dst}" --time-cond "${dst}" --user-agent "${USER_AGENT}" "${url}" && {
     {
       echo '<?xml-stylesheet type="text/xsl" href="../assets/schedule2html.xslt"?>'
@@ -81,8 +81,8 @@ git add . && git commit -a -m '🚀'
 
 dir="wiki"
 
-# url="https://events.ccc.de/${evt}/2015/${dir}/version"
-url="https://events.ccc.de/${evt}/${year}/${dir}/version"
+# url="https://events.ccc.de/${evet}/2015/${dir}/version"
+url="https://events.ccc.de/${evet}/${year}/${dir}/version"
 dst="${dir}.version"
 
 curl --silent --location --remote-time --output "${dst}" --time-cond "${dst}" --user-agent "${USER_AGENT}" "${url}" && {
